caf2e98c576fd1282805d61416732c4f3e3130b7
alan-ps <serhiipuchkovskii@gmail.com>
2020-04-10 23:24:43 +0300

    Move theme declaration to the top. Use theme variables for the colors.

--- src/Pages/App.jsx
+++ src/Pages/App.jsx
@@ -2,18 +2,32 @@ import React from 'react';
 import {
   BrowserRouter as Router, Route, Switch,
 } from 'react-router-dom';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import HomePage from './HomePage/HomePage';
 import AdminApp from './AdminApp';
+import Footer from '../Components/Template/Footer';
 import '../index.css';
+
+const customTheme = createMuiTheme({
+  palette: {
+    primary: { main: '#1540A4' },
+    text: { secondary: '#B9C6E4' }
+  },
+});
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/admin" component={AdminApp} />
-      </Switch>
-    </Router>
+    <ThemeProvider theme={customTheme}>
+      <CssBaseline />
+      <Router>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/admin" component={AdminApp} />
+        </Switch>
+      </Router>
+      <Footer />
+    </ThemeProvider>
   );
 }
 
