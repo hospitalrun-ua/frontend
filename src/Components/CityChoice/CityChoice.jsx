5225b2e1986fdccfc2e2972f77c5791f5b6d61ea
IhorP <eldhim@gmail.com>
2020-08-17 08:41:59 +0300

    [oknelvapi] remove junk

--- src/Components/CityChoice/CityChoice.jsx
+++ src/Components/CityChoice/CityChoice.jsx
@@ -3,11 +3,9 @@ import { markerIcon } from '../../assets/icons';
 import i18n from '../../i18n';
 import './CityChoice.css';
 
-
 const { cityChoice } = i18n.header;
 
 const CityChoice = () => {
-
   return (
     <div className="cityChoiceWrap">
       {markerIcon}
